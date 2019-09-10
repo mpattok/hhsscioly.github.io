826e25699d94f4dd9ac001318e2144d5262e77c6
hhsscioly <54913666+hhsscioly@users.noreply.github.com>
2019-09-10 09:07:41 -0400

    Update event-builder.js

--- events/event-builder.js
+++ events/event-builder.js
@@ -9,7 +9,11 @@ title = title.replace("&","and").replace(/ /g,"-");
 content = content + `<a href="https://www.soinc.org/`+title.toLowerCase().replace("of-","")+`-c" target="_blank">Official Science Olympiad page for this event</a><br>
 <a href="https://scioly.org/wiki/index.php/`+title.replace(/-/g,"_")+`" target="_blank">Science Olympiad Student Center page for this event</a>
 <h3><a target="_blank" href="https://drive.google.com/open?id=`+resourceId+`">Event Resources (click here to view in a separate tab):</a></h3>
+<div class="iframeDiv">
 <iframe src="https://drive.google.com/embeddedfolderview?id=`+resourceId+`#grid" width="99%" height="500">An error occured. Click the link above to view the resources for this event in a separate tab.</iframe>
+</div>
 <h3><a target="_blank" href="rules/`+title.toLowerCase()+`-rules.pdf">Rules (click here to view PDF in separate tab):</a></h3>
-<iframe width="99%" height="850px" src="rules/`+title.toLowerCase()+`-rules.pdf">An error occured. Click the link above to view the rules in a separate tab.</iframe>`;
+<div class="iframeDiv">
+<iframe width="99%" height="850px" src="rules/`+title.toLowerCase()+`-rules.pdf">An error occured. Click the link above to view the rules in a separate tab.</iframe>
+</div>`;
 title = title.replace(/-/g," ").replace("and","&");
