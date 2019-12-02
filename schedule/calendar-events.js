4effce621b7a653037d2d511de82e825a2c93ddd
hhsscioly <54913666+hhsscioly@users.noreply.github.com>
2019-12-02 14:28:10 -0500

    Update calendar-events.js

--- schedule/calendar-events.js
+++ schedule/calendar-events.js
@@ -3,4 +3,17 @@
   TRUE/FALSE refers to whether or not an event goes on list schedule (joke items like "Wish Matt a happy birthday :)" would have false for that value)
 */
 
-var events = [["9-10-2019","<b>First meeting</b><br>",true],["10-31-2019","Spooky Time",false],["11-11-2019","Wish Matt a happy birthday :)",false],["12-11-2019","<b>Pizza Resource Night</b>",true],["12-14-2019","<b>Portage Invitational</b>",true],["1-18-2020","<b>Haslett Invitational</b>",true],["2-1-2020","<b>WOSO Invitional</b>",true],["2-14-2020","<b>U of M Invitational</b><br>Staying the night",true],["2-15-2020","<b>U of M Invitational</b><br>Staying the night",true],["3-21-2020","<b>WMU - Regionals</b><br>Maybe the 28th?",true],["3-28-2020","<b>WMU - Regionals</b><br>Maybe the 21st?",true],["4-25-2019","<b>States</b><br>(If we make it)",true]];
+var events = [
+  ["9-10-2019","<b>First meeting</b><br>",true],
+  ["10-31-2019","Spooky Time",false],
+  ["11-11-2019","Wish Matt a happy birthday :)",false],
+  ["12-11-2019","<b>Pizza Resource Night</b>",true],
+  ["12-14-2019","<b>Portage Invitational</b>",true],
+  ["1-18-2020","<b>Haslett Invitational</b>",true],
+  ["2-1-2020","<b>WOSO Invitional</b><br>If we do it, only one team will go",true],
+  ["2-7-2020","<b>Frankenmuth Invitational</b><br>Staying night in hotel Friday night, competition Saturday",true],
+  ["2-8-2020","<b>Frankenmuth Invitational</b><br>Staying night in hotel Friday night, competition Saturday",true],
+  ["2-22-2020","<b>Allendale Invitational</b>",true],
+  ["3-21-2020","<b>WMU - Regionals</b>",true],
+  ["4-25-2019","<b>States</b><br>If we make it top 17 kids go. You can leave early after your events for prom.",true]
+];
